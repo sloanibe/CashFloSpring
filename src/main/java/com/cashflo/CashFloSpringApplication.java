6859288691d70dcccc8fc18dfab97628e35e946e
sloanibe <sloanibe@gmail.com>
2018-01-26 11:30:54 -0800

    Initial commit

---
+++ src/main/java/com/cashflo/CashFloSpringApplication.java
@@ -0,0 +1,12 @@
+package com.cashflo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFloSpringApplication.class, args);
+	}
+}
